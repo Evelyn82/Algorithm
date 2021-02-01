33e6958be1aad9e66137cc204ace5daa1f5f3162
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-02-01 12:49:12 +0900

    Update LCP(Longest Common Prefix).cpp

--- String/LCP(Longest Common Prefix).cpp
+++ String/LCP(Longest Common Prefix).cpp
@@ -1,8 +1,11 @@
 void LCP(){
     for(int i=0, k=0;i<n;++i, k=max(k-1, 0)){
         if(pos[i]==0) continue;
+        // if(pos[i]==n-1) continue; 뒤와 비교할 경우
         
         for(int j=sa[pos[i]-1];s[i+k]==s[j+k];++k);
+        //for(int j=sa[pos[i]+1]...) 뒤와 비교할 경우
+        
         lcp[pos[i]]=k;
     }
     printf("x ");
