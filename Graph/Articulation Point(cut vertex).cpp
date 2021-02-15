a290fca5d04b8150012ad851aee0716aac3c30ab
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-02-15 14:56:30 +0900

    Update Articulation Point(cut vertex).cpp

--- Graph/Articulation Point(cut vertex).cpp
+++ Graph/Articulation Point(cut vertex).cpp
@@ -1,14 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <memory.h>
-using namespace std;
-
 const int MAXV = 1e4 + 1;
 int V, E, discovered[MAXV], counter = 1;
-bool visited[MAXV];
 vector<int> v[MAXV];
 vector<int> isCutVertex;
+
 int findCutVertex(int n, bool isRoot) {
 	discovered[n] = counter++;
 	int ret = discovered[n];
@@ -16,7 +10,7 @@ int findCutVertex(int n, bool isRoot) {
 	int children = 0;
 	for (int k = 0; k < v[n].size(); k++) {
 		int next = v[n][k];
-		if (discovered[next] == -1) {
+		if (!discovered[next]) {
 			children++;
 			int subTree = findCutVertex(next, false);
 			if (!isRoot && subTree >= discovered[n]) {
@@ -29,29 +23,13 @@ int findCutVertex(int n, bool isRoot) {
 	return ret;
 }
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> V >> E;
-	int a, b;
-	for (int k = 0; k < E; k++) {
-		cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-		visited[a] = true;
-		visited[b] = true;
-	}
-	memset(discovered, -1, sizeof(discovered));
+	
 	for (int i = 1; i < MAXV; i++) {
-		if (visited[i] && discovered[i] == -1) {
+		if (!discovered[i]) {
 			counter = 1;
 			findCutVertex(i, true);
 		}
 	}
 	sort(isCutVertex.begin(), isCutVertex.end());
 	isCutVertex.erase(unique(isCutVertex.begin(), isCutVertex.end()), isCutVertex.end());
-	cout << isCutVertex.size() << '\n';
-	if (isCutVertex.size() != 0) {
-		for (int num : isCutVertex) cout << num << ' ';
-		cout << '\n';
-	}  
 }
