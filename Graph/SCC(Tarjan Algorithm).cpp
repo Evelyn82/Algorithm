d09819f5313fd6edc15277f19c54b19dd945c409
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-02-15 14:57:35 +0900

    Update and rename SCC, Tarjan Algorithm.cpp to SCC(Tarjan Algorithm).cpp

--- Graph/SCC(Tarjan Algorithm).cpp
+++ Graph/SCC(Tarjan Algorithm).cpp
@@ -1,16 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <memory.h>
-#include <algorithm>
-using namespace std;
-
 const int MAXV = 10010;
 int V, E, sccCounter, vertexCounter;
 vector<int> adj[MAXV];
 stack<int> st;
 vector<vector<int>> result;
 int sccId[MAXV], discovered[MAXV];
+
 int SCC(int num) {
 	int ret = discovered[num] = vertexCounter++;
 	st.push(num);
@@ -37,24 +31,8 @@ int SCC(int num) {
 	}return ret;
 }
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> V >> E;
-	int a, b;
-	while (E--) {
-		cin >> a >> b;
-		adj[a].push_back(b);
-	}
 	sccCounter = vertexCounter = 1;
 	for (int i = 1; i <= V; i++) {
 		if (!discovered[i]) SCC(i);
 	}
-	sort(result.begin(), result.end());
-	cout << result.size() << '\n';
-	for (vector<int>v : result) {
-		for (int n : v) {
-			cout << n << ' ';
-		}cout << "-1\n";
-	}
-	cout << '\n';
 }
